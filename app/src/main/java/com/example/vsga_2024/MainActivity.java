09a3d7771af2abbd5cce7320ed110f2847bf52c7
kfahmi77 <khoirulfahmi44@gmail.com>
2024-07-26 17:24:36 +0700

    create function create,view and delete data from internal storage

--- app/src/main/java/com/example/vsga_2024/MainActivity.java
+++ app/src/main/java/com/example/vsga_2024/MainActivity.java
@@ -1,24 +1,84 @@
 package com.example.vsga_2024;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String FILE_NAME = "vsga_2024.txt";
+    Button createFileBtn, modifyFileBtn, readFileBtn, deleteFileBtn;
+    EditText resultTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        resultTxt = findViewById(R.id.resultTxt);
     }
+
+    private void createFile() {
+        File file = new File(getFilesDir(), FILE_NAME);
+        FileOutputStream fos;
+        try {
+            file.createNewFile();
+            fos = new FileOutputStream(file, false);
+            fos.write(resultTxt.getText().toString().getBytes());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readFile() {
+        File file = new File(getFilesDir(), FILE_NAME);
+        if (file.exists()) {
+            StringBuilder text = new StringBuilder();
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                while (line != null) {
+                    text.append(line);
+                    line = br.readLine();
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            resultTxt.setText(text);
+        }
+        else {
+            resultTxt.setText("");
+        }
+    }
+
+    private void deleteFile() {
+        File file = new File(getFilesDir(), FILE_NAME);
+        if (file.exists()) {
+            file.delete();
+            resultTxt.setText("");
+        }
+    }
+    public void onClick(View view) {
+        if (view.getId() == R.id.createFileBtn)
+            createFile();
+        else if (view.getId() == R.id.readFileBtn)
+            readFile();
+        else if (view.getId() == R.id.deleteFileBtn)
+            deleteFile();
+    }
+
 }
